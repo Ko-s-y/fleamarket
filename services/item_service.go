7721997880036631364dcdd7973cb50a24822ed4
s-ko <song.1209@icloud.com>
2025-03-02 12:47:26 +0900

    feat: service/item定義

---
+++ services/item_service.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"fleamarket/models"
+	"fleamarket/repositories"
+)
+
+type IItemService interface {
+	FindAll() (*[]models.Item, error)
+}
+
+type ItemService struct {
+	repository repositories.IItemRepository
+}
+
+func NewItemService(repository repositories.IItemRepository) IItemService {
+	return &ItemService{
+		repository: repository,
+	}
+}
+
+func (s *ItemService) FindAll() (*[]models.Item, error) {
+	return s.repository.FindAll()
+}
