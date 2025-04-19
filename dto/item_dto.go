712f4317e365bafcee9606208c1b7167f4784407
s-ko <song.1209@icloud.com>
2025-04-20 00:32:42 +0900

    feat: binding: minのスペース削除

--- dto/item_dto.go
+++ dto/item_dto.go
@@ -1,8 +1,8 @@
 package dto
 
 type CreateItemInput struct {
-	Name        string `json:"name" binding:"required, min=3"`
-	Price       uint   `json:"price" binding:"required, min=1,max=1000000"`
+	Name        string `json:"name" binding:"required,min=3"`
+	Price       uint   `json:"price" binding:"required,min=1,max=1000000"`
 	Description string `json:"description"`
 }
 
