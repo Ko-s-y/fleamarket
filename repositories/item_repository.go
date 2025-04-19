b3dc51d73afa61c35d3818ea057a8ee883304675
s-ko <song.1209@icloud.com>
2025-04-20 00:03:16 +0900

    feat: ItemRepository層にUpdateを定義

--- repositories/item_repository.go
+++ repositories/item_repository.go
@@ -9,6 +9,7 @@ type IItemRepository interface {
 	FindAll() (*[]models.Item, error)
 	FindById(id uint) (*models.Item, error)
 	Create(item models.Item) (*models.Item, error)
+	Update(item models.Item) (*models.Item, error)
 }
 
 type ItemMemoryRepository struct {
@@ -39,4 +40,14 @@ func (r *ItemMemoryRepository) Create(newItem models.Item) (*models.Item, error)
 	newItem.ID = uint(len(r.items) + 1)
 	r.items = append(r.items, newItem)
 	return &newItem, nil
+}
+
+func (r *ItemMemoryRepository) Update(item models.Item) (*models.Item, error) {
+	for i, v := range r.items {
+		if v.ID == item.ID {
+			r.items[i] = item
+			return &r.items[i], nil
+		}
+	}
+	return nil, errors.New("Unexpected error")
 }
