3f23418f7e47906aa5e0f4dc84fbc8aee466beb8
s-ko <song.1209@icloud.com>
2025-04-30 23:16:01 +0900

    feat: itemモデルの定義をgormを利用する

--- models/item.go
+++ models/item.go
@@ -1,9 +1,11 @@
 package models
 
+import "gorm.io/gorm"
+
 type Item struct {
-	ID          uint   `json:"id"`
-	Name        string `json:"name"`
-	Price       uint   `json:"price"`
-	Description string `json:"description"`
-	SoldOut     bool   `json:"sold_out"`
+	gorm.Model
+	Name        string `gorm:"not null"`
+	Price       uint   `gorm:"not null"`
+	Description string
+	SoldOut     bool `gorm:"not null;default:false"`
 }
